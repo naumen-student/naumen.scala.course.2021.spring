35d25fd6147eb9e89ca01feb7c853ec28e4c92ed
PesGospoda <kanniball@bk.ru>
2021-03-15 01:12:52 +0500

    homeWork_1 complete

---
+++ homeworks/homework_1/task_3/src/main/scala/main.scala
@@ -0,0 +1,11 @@
+class main extends App {
+  val a = "Helloo Scala!! This is me, Alexey, Alexey Chermnykh"
+  println(a)
+
+  println(a + "!!!!")
+  val Hola = "Not hola"
+  val Alexey = ", you are not Alexey! "
+  println(Hola, Alexey, Hola)
+  println(Hola + Alexey + Hola)
+
+}
