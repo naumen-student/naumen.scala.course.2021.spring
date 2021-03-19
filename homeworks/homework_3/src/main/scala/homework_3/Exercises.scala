3b92392da9712e77e3c5d53de0c273d370f3ffc1
off-color <qweslark@yandex.ru>
2021-03-19 12:42:51 +0500

    hw3

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -1,5 +1,7 @@
 package homework_3
 
+import scala.runtime.Nothing$
+
 object Exercises {
 
 
@@ -12,11 +14,25 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
      * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
      */
-    def prettyBooleanFormatter1(x: Any): String = ???
+    def prettyBooleanFormatter1(x: Any): String = {
+        if (x.isInstanceOf[Boolean]) formatBoolean(x.asInstanceOf[Boolean])
+        else x.toString
+    }
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+    def prettyBooleanFormatter2(x: Any): String = x match {
+        case bool: Boolean => formatBoolean(bool)
+        case _ => x.toString
+    }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+    def prettyBooleanFormatter3(x: Any): String = {
+        if (x.getClass == Class.forName("java.lang.Boolean")) formatBoolean(x.asInstanceOf[Boolean])
+        else x.toString
+    }
+
+    def formatBoolean(x: Boolean): String = x match {
+        case true => "правда"
+        case _ => "ложь"
+    }
 
 
     /**
@@ -26,11 +42,20 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
      * Обратите внимание на возвращаемые типы.
      */
-    def max1(xs: Seq[Int]): Int = ???
+    def max1(xs: Seq[Int]): Int = xs.length match {
+        case 0 => throw new IllegalArgumentException("xs length was zero")
+        case _ => xs.max
+    }
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+    def max2(xs: Seq[Int]): Seq[Int] = xs.length match {
+        case 0 => Seq()
+        case _ => Seq(xs.max)
+    }
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+    def max3(xs: Seq[Int]): Option[Int] = xs.length match {
+        case 0 => Option.empty
+        case _ => Option(xs.max)
+    }
 
     /**
      * Задание №3
@@ -42,8 +67,11 @@ object Exercises {
      * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
      * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
      */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
+    def sum1(x: Int, y: Int): Int = sumIntegers(List(x, y))
+    def sum2(x: Int, y: Int): Int = sumIntegers(Seq(x, y))
+    def sum3(x: Int, y: Int): Int = sumIntegers(IntPair(x, y))
 
+    case class IntPair(x: Int, y: Int) extends Iterable[Int] {
+        override def iterator: Iterator[Int] = Iterator(x, y)
+    }
 }
--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,58 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
+        'test_prettyBooleanFormatter_forTrue - {
             val trueStr = "правда"
+
             assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter2(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter3(true) == trueStr)
+        }
+        'test_prettyBooleanFormatter_forFalse - {
+            val falseStr = "ложь"
+
+            assert(Exercises.prettyBooleanFormatter1(false) == falseStr)
+            assert(Exercises.prettyBooleanFormatter2(false) == falseStr)
+            assert(Exercises.prettyBooleanFormatter3(false) == falseStr)
+        }
+        'test_prettyBooleanFormatter_forNotBoolean - {
+            val notBoolean = 10
+
+            assert(Exercises.prettyBooleanFormatter1(notBoolean) == notBoolean.toString)
+            assert(Exercises.prettyBooleanFormatter2(notBoolean) == notBoolean.toString)
+            assert(Exercises.prettyBooleanFormatter3(notBoolean) == notBoolean.toString)
+        }
+        'test_max_manyElementsSeq - {
+            val sequence = Seq(0, 10, 20, 30)
+
+            assert(Exercises.max1(sequence) == 30)
+            assert(Exercises.max2(sequence) == Seq(30))
+            assert(Exercises.max3(sequence) == Option(30))
+        }
+        'test_max_oneElementSeq - {
+            val sequence = Seq(1)
+
+            assert(Exercises.max1(sequence) == 1)
+            assert(Exercises.max2(sequence) == Seq(1))
+            assert(Exercises.max3(sequence) == Option(1))
+        }
+        'test_max_emptySeq - {
+            val sequence = Seq()
+
+            intercept[IllegalArgumentException] {
+                Exercises.max1(sequence)
+            }
+            assert(Exercises.max2(sequence) == Seq())
+            assert(Exercises.max3(sequence).isEmpty)
+        }
+        'test_sum - {
+            val cases = Seq((1, 1, 2), (-1, 1, 0), (100, 50, 150))
+
+            cases.foreach(c => {
+                assert(Exercises.sum1(c._1, c._2) == c._3)
+                assert(Exercises.sum2(c._1, c._2) == c._3)
+                assert(Exercises.sum3(c._1, c._2) == c._3)
+            })
         }
     }
 }
