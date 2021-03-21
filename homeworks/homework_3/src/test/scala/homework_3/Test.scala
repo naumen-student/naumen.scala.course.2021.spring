81efc501dfd1163d5f4dbf4b4603423eb9634832
Марина Коновалова <konovalova.owl@gmail.com>
2021-03-22 03:10:37 +0500

    add homework_3

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,51 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
+        'test_prettyBooleanFormatter - {
             val trueStr = "правда"
+            val falseStr = "ложь"
             assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter1(false) == falseStr)
+            assert(Exercises.prettyBooleanFormatter1(1) == "1")
+            assert(Exercises.prettyBooleanFormatter1("some_string") == "some_string")
+
+            assert(Exercises.prettyBooleanFormatter2(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter2(false) == falseStr)
+            assert(Exercises.prettyBooleanFormatter2(1) == "1")
+            assert(Exercises.prettyBooleanFormatter2("some_string") == "some_string")
+
+            assert(Exercises.prettyBooleanFormatter3(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter3(false) == falseStr)
+            assert(Exercises.prettyBooleanFormatter3(1) == "1")
+            assert(Exercises.prettyBooleanFormatter3("some_string") == "some_string")
+
+        }
+        'test_max - {
+            assert(Exercises.max1(Seq(1, 2, 3, 4, 5)) == 5)
+            assert(Exercises.max1(Seq(1)) == 1)
+            assert(Exercises.max1(Seq()) == 0)
+
+            assert(Exercises.max2(Seq(1, 2, 3, 4, 5)) == Seq(5))
+            assert(Exercises.max2(Seq(1)) == Seq(1))
+            assert(Exercises.max2(Seq()) == Seq())
+
+            assert(Exercises.max3(Seq(1, 2, 3, 4, 5)) == Some(5))
+            assert(Exercises.max3(Seq(1)) == Some(1))
+            assert(Exercises.max3(Seq()) == None)
+        }
+
+        'test_sum - {
+            assert(Exercises.sum1(1, 2) == 3)
+            assert(Exercises.sum1(0, 1) == 1)
+            assert(Exercises.sum1(-1, 2) == 1)
+
+            assert(Exercises.sum2(1, 2) == 3)
+            assert(Exercises.sum2(0, 1) == 1)
+            assert(Exercises.sum2(-1, 2) == 1)
+
+            assert(Exercises.sum3(1, 2) == 3)
+            assert(Exercises.sum3(0, 1) == 1)
+            assert(Exercises.sum3(-1, 2) == 1)
         }
     }
 }
