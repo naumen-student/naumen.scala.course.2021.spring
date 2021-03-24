a28e55277da5db392da8ba7cbd11ce58702b6b58
koksharov1998@ya.ru <koksharov1998@ya.ru>
2021-03-24 19:18:15 +0500

    completed homework_4

--- homeworks/homework_4/src/main/scala/Cell.scala
+++ homeworks/homework_4/src/main/scala/Cell.scala
@@ -11,19 +11,26 @@ case class StringCell(string: String) extends Cell {
 }
 case class ReferenceCell(ix: Int, iy: Int, table: Table) extends Cell {
   override def toString: String = table.getCell(ix, iy) match {
+    // without deep recursion
     // case Some(cell: ReferenceCell) => if (cell.table.getCell(cell.ix, cell.iy) == Some(this)) "cyclic" else cell.toString
+
     // what's about deep recursion???
     case Some(cell: ReferenceCell) => {
       var cyclic = false
-      //var not_reference = false
+      var not_reference = false
       var new_cell = cell.table.getCell(cell.ix, cell.iy)
-      while (new_cell.isInstanceOf[Option[ReferenceCell]] && !cyclic) {
-        val new_ref_cell = new_cell.asInstanceOf[Option[ReferenceCell]].get
-        if (new_ref_cell.table == this.table && new_ref_cell.ix == this.ix && new_ref_cell.iy == this.iy)
-          cyclic = true
-        new_cell = new_ref_cell.table.getCell(new_ref_cell.ix, new_ref_cell.iy)
+      while (!not_reference && !cyclic) {
+        new_cell match {
+          case Some(new_ref_cell: ReferenceCell) => {
+            if (new_ref_cell.table == this.table && new_ref_cell.ix == this.ix && new_ref_cell.iy == this.iy)
+              cyclic = true
+            else
+              new_cell = new_ref_cell.table.getCell(new_ref_cell.ix, new_ref_cell.iy)
+          }
+          case _ => not_reference = true
+        }
       }
-      if (cyclic) "cyclic" else new_cell.toString
+      if (cyclic) "cyclic" else new_cell.get.toString
     }
     case Some(cell) => cell.toString
     case None => "outOfRange"
