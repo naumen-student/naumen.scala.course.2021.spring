00d2d54663879f7fe3a55d90582ea00feae3a006
Konstantin Kolmogortsev <mr.kolmogortsev@gmail.com>
2021-03-25 10:31:34 +0500

    homework_3 Kolmogortsev Konstantin

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -12,11 +12,37 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
      * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
      */
-    def prettyBooleanFormatter1(x: Any): String = ???
+    def prettyBooleanFormatter1(x: Any): String = {
+        var res : String = ""
+        if(x.isInstanceOf[Boolean]){
+            if(x.asInstanceOf[Boolean])
+                res = "правда"
+            else
+                res = "ложь"
+        }
+        else res = x.toString
+        res
+    }
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+    def prettyBooleanFormatter2(x: Any): String = {
+        x match {
+            case true => "правда"
+            case false => "ложь"
+            case _ => x.toString
+        }
+    }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+    def prettyBooleanFormatter3(x: Any): String = {
+        var res : String = ""
+        if(x.getClass == Class.forName("java.lang.Boolean")){
+            if(x.asInstanceOf[Boolean])
+                res = "правда"
+            else
+                res = "ложь"
+        }
+        else res = x.toString
+        res
+    }
 
 
     /**
@@ -26,11 +52,24 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
      * Обратите внимание на возвращаемые типы.
      */
-    def max1(xs: Seq[Int]): Int = ???
+    def max1(xs: Seq[Int]): Int = {
+        if(xs.length == 0){
+            throw new Exception("sequence is empty")
+        }
+        else xs.max
+    }
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+    def max2(xs: Seq[Int]): Seq[Int] = {
+        if(xs.length == 0){
+            Seq()
+        }
+        else Seq(xs.max)
+    }
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+    def max3(xs: Seq[Int]): Option[Int] = {
+      if (xs.length == 0) Option.empty
+      else Option(xs.max)
+    }
 
     /**
      * Задание №3
@@ -42,8 +81,12 @@ object Exercises {
      * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
      * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
      */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
+    def sum1(x: Int, y: Int): Int = sumIntegers(Seq(x,y))
+    def sum2(x: Int, y: Int): Int = sumIntegers(List(y,x))
 
+    class MyClass[A](x: A, y: A) extends Iterable[A] {
+      override def iterator: Iterator[A] = Iterator(x, y)
+    }
+
+    def sum3(x: Int, y: Int): Int = sumIntegers(new MyClass[Int](x,y))
 }
--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,56 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        'test_prettyBooleanFormatter1 - {
+            assert(Exercises.prettyBooleanFormatter1(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter1(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter1(25.6) == "25.6")
+            assert(Exercises.prettyBooleanFormatter2(25) == "25")
+        }
+
+        'test_prettyBooleanFormatter2 - {
+            assert(Exercises.prettyBooleanFormatter2(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter2(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter2(25.6) == "25.6")
+            assert(Exercises.prettyBooleanFormatter2(25) == "25")
+        }
+        'test_prettyBooleanFormatter3 - {
+            assert(Exercises.prettyBooleanFormatter3(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter3(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter3(25.6) == "25.6")
+            assert(Exercises.prettyBooleanFormatter3(25) == "25")
+        }
+
+        'test_max1- {
+            assert(Exercises.max1(Seq(1,5,7,3,4,2,2)) == 7)
+            intercept[Exception] {
+                Exercises.max1(Seq())
+            }
+            assert(Exercises.max1(Seq(67)) == 67)
+
+
+        }
+        'test_max2- {
+            assert(Exercises.max2(Seq(1,5,7,3,4,2,2)) == Seq(7))
+            assert(Exercises.max2(Seq()) == Seq())
+            assert(Exercises.max2(Seq(67)) == Seq(67))
+        }
+        'test_max3- {
+            assert(Exercises.max3(Seq(1,5,7,3,4,2)) == Option(7))
+            assert(Exercises.max3(Seq()).isEmpty)
+            assert(Exercises.max3(Seq(67)) == Option(67))
+        }
+        'test_sum1- {
+            assert(Exercises.sum1(6,1) == 7)
+            assert(Exercises.sum1(0,0) == 0)
+        }
+        'test_sum2- {
+            assert(Exercises.sum2(6,1) == 7)
+            assert(Exercises.sum2(0,0) == 0)
+        }
+        'test_sum3- {
+            assert(Exercises.sum2(6,1) == 7)
+            assert(Exercises.sum2(0,0) == 0)
         }
     }
 }
