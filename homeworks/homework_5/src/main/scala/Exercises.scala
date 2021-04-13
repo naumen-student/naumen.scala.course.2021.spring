1f01166f1a9a2a66b7676a3901916c7b64ac3ff1
Кирилл Турышев <capricorn.desu@yandex.ru>
2021-04-13 13:09:01 +0500

    homework 5

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -1,3 +1,6 @@
+import scala.collection.immutable.List
+
+
 object Exercises {
   trait Animal {
     def name: String
@@ -9,15 +12,56 @@ object Exercises {
 
 
 
-  case class Shelter ...
+  case class Shelter[+T <: Animal](list : List[T])
+  {
+    def +[U >: T <: Animal](animal: U): Shelter[U] = {
+    //def +[U <: Animal](animal: U): Shelter[U] = {
+      //if (animal.isInstanceOf[T])
+      Shelter(animal :: list)
+      //else
+        //Shelter(animal.asInstanceOf[Animal] :: list.asInstanceOf[List[Animal]])
+    }
+
+    def ++[U >: T <: Animal](shelter: Shelter[U]): Shelter[U] = {
+    //def ++[U <: Animal](shelter: Shelter[U]): Shelter[U] = {
+      /*if (shelter.isInstanceOf[Shelter[T]])
+        Shelter(list ::: shelter.list)
+      else
+        Shelter(shelter.list.asInstanceOf[List[Animal]] ::: list.asInstanceOf[List[Animal]])*/
+      Shelter(list ::: shelter.list)
+    }
+
+    def getNames: List[String] = {
+      list.map(_.name)
+    }
+
+    def feed(food: Food[T]): List[String] = list.map(food.feed)
+  }
 
 
 
-  trait Food ...
+  trait Food[-T]{
+      def feed(animal: T): String
+  }
 
-  case object Meat extends Food[Animal] ...
+  case object Meat extends Food[Animal] {
+    override def feed(animal: Animal): String =
+    {
+      animal.name + " eats meat"
+    }
+  }
 
-  case object Milk extends Food[Cat] ...
+  case object Milk extends Food[Cat] {
+    override def feed(animal: Cat): String =
+    {
+      animal.name + " eats milk"
+    }
+  }
 
-  case object Bread extends Food[Dog] ...
+  case object Bread extends Food[Dog] {
+    override def feed(animal: Dog): String =
+    {
+      animal.name + " eats bread"
+    }
+  }
 }
