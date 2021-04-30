ec46f1bb9466f3f3f6484e380616139d942f617c
Igor' Kantimirov <igorkantimirov@gmail.com>
2021-04-30 16:59:09 +0500

    Add homework_6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,16 @@
+import scala.annotation.tailrec
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    @tailrec
+    def loop(seq: Seq[T], resultSeq: Seq[T]): Seq[T] = seq match {
+      case Nil => resultSeq
+      case x :: y => loop(y, x +: resultSeq)
+    }
+    loop(seq, Seq())
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +18,28 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    @tailrec
+    def loop(index: Int, s: Int, current: Int): Int = {
+      if (index <= 0) current
+      else loop(index - 1, s + current, s)
+    }
+    loop(idx, 1, 0)
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    var prev = -1
+    var next = 1
+    var s = Seq[Int]()
+    for(_ <- 0 to idx)
+    {
+      val sum = prev + next
+      prev = next
+      next = sum
+      s = s :+ sum
+    }
+    s
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +47,26 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String =
+    text.map(c => MORSE.getOrElse(c.toUpper.toString, c.toString)).mkString(" ")
 
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = {
+    val temp = new StringBuilder
+    val result = new StringBuilder
+    for (ch <- text.toCharArray) {
+      if (ch.isLetter) temp.append(ch)
+      else {
+        var r = temp.result().reverse
+        if(r.nonEmpty && r(r.length - 1).isUpper) {
+          r = r(0).toUpper + r.substring(1, r.length-1) + r(r.length - 1).toLower
+        }
+        result.append(r).append(ch)
+        temp.delete(0, temp.length)
+      }
+    }
+    result.append(temp)
+    result.toString
+  }
 
 }
