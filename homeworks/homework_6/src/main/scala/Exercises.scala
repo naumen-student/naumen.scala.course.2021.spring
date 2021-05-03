7a3a1e180aa71d9050b026300b81c576737efe5d
ChizhovAlexey <43272109+ChizhovAlexey@users.noreply.github.com>
2021-05-03 16:21:11 +0500

    tasks 1-4

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,10 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq match {
+    case head :: tail => reverse(tail) :+ head
+    case _ => Nil
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +12,16 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = idx match {
+    case 0 => 0
+    case 1 => 1
+    case _ => fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = idx match {
+    case 0 => Seq(0)
+    case _ => fibonacci(idx - 1) :+ fibonacci4Index(idx)
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +29,24 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String =
+  text.map(c => MORSE.getOrElse(c.toString.toUpperCase, c.toString))
+    .mkString(" ")
 
+  def getTokens(text: String): List[String] = text match {
+    case "" => List[String]()
+    case _ => text.takeWhile(_.isLetter == text.charAt(0).isLetter).mkString("") ::
+      getTokens(text.dropWhile(_.isLetter == text.charAt(0).isLetter))
+  }
 
-  def wordReverse(text: String): String = ???
+  def mapToken(tkn: String): String = tkn.charAt(0) match {
+    case x if !x.isLetter => tkn
+    case x if x.isUpper => tkn.toLowerCase.reverse.capitalize
+    case _ => tkn.reverse
+  }
+
+  def wordReverse(text: String): String = getTokens(text)
+    .map(mapToken)
+    .mkString("")
 
 }
