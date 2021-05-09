5fc1f43cd8f699d711824e9526fe756e00f66c89
Yegor Druzhinin <egor.druzhinin97@gmail.com>
2021-05-09 22:27:40 +0500

    Hw done

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,27 +1,51 @@
+import scala.annotation.tailrec
+
 object Exercises {
 
+    lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
+        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+        "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+        "Y" -> "-.--", "Z" -> "--..")
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+    def reverse[T](seq: Seq[T]): Seq[T] = seq.foldRight(Seq[T]())((elem, seq) => seq :+ elem)
 
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
+    def fibonacci4Index(idx: Int): Int = {
+        @tailrec
+        def fib(a: Int, b: Int, n: Int): Int = {
+            n match {
+                case 0 => a
+                case _ => fib(b, a + b, n - 1)
+            }
+        }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+        fib(0, 1, idx)
+    }
 
-  lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    def fibonacci(idx: Int): Seq[Int] = {
+        @tailrec
+        def fib(current: Seq[Int], n: Int): Seq[Int] = {
+            n match {
+                case 1 => current
+                case _ => fib(current :+ current.reverseIterator.take(2).sum, n - 1)
+            }
+        }
 
-  def morse(text: String): String = ???
+        idx match {
+            case 0 => Seq(0)
+            case _ => fib(Seq(0, 1), idx)
+        }
+    }
 
+    def morse(text: String): String = text.map(ch => MORSE.getOrElse(ch.toUpper.toString, ch.toString)).mkString(" ")
 
-  def wordReverse(text: String): String = ???
+    def wordReverse(text: String): String = {
+        def reverse(s: String): String = {
+            val (word, delimiters) = s.span(x => x.isLetter)
 
+            (if (word(0).isUpper) word.reverse.toLowerCase.capitalize else word.reverse).concat(delimiters)
+        }
+
+        text.split(' ').map(reverse).mkString(" ")
+    }
 }
--- homeworks/homework_6/src/test/scala/Test.scala
+++ homeworks/homework_6/src/test/scala/Test.scala
@@ -1,33 +1,37 @@
 import utest._
 
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-  val tests = Tests{
-    'reverse - {
-      assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
-      assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
+    val tests = Tests {
+        'reverse - {
+            assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
+            assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
+        }
+
+        'fibonacci4Index - {
+            assert(Exercises.fibonacci4Index(0) == 0)
+            assert(Exercises.fibonacci4Index(1) == 1)
+
+            assert(Exercises.fibonacci4Index(2) == 1)
+            assert(Exercises.fibonacci4Index(5) == 5)
+        }
+
+        'fibonacci - {
+            assert(Exercises.fibonacci(0) == Seq(0))
+            assert(Exercises.fibonacci(1) == Seq(0, 1))
+
+            assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
+            assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
+        }
+
+        'morse - {
+            assert(Exercises.morse("SOS") == "... --- ...")
+            assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -.. !")
+        }
+
+        'wordReverse - {
+            assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
+        }
     }
-
-    'fibonacci4Index - {
-      assert(Exercises.fibonacci4Index(2) == 1)
-      assert(Exercises.fibonacci4Index(5) == 5)
-    }
-
-    'fibonacci - {
-      assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
-      assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
-    }
-
-    'morse - {
-      assert(Exercises.morse("SOS") == "... --- ...")
-      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
-    }
-
-    'wordReverse - {
-      assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
-    }
-
-
-  }
 }
